b62661fc7afee2589211e15065c4af8cd99fd36b
Simeon Tran <siatran@ucsc.edu>
2023-08-16 17:40:54 -0700

    Simple salt by taking UUID for each user and removing dashes

--- src/system/db/sql/data.sql
+++ src/system/db/sql/data.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-INSERT INTO auth (id, email, passwordhash, salt, scopes) VALUES ('6e007ce8-7a29-470a-9d87-c2dae1d9728e', 'simeonat@notrealemail.com', crypt('simeonat', '66ce678d7f0f0025a95b3aaebc5ee29c'), '66ce678d7f0f0025a95b3aaebc5ee29c', jsonb_build_array('user'));
-INSERT INTO auth (id, email, passwordhash, salt, scopes) VALUES ('4b456188-75c5-4dc8-b644-7d42490cfd52', 'rocketraccoon@guardians.com', crypt('raccoon', '9ec8f370f2a5e5141afd4f1802d8dabb'), '9ec8f370f2a5e5141afd4f1802d8dabb', jsonb_build_array('user', 'admin'));
+INSERT INTO auth (id, email, passwordhash, salt, scopes) VALUES ('6e007ce8-7a29-470a-9d87-c2dae1d9728e', 'simeonat@notrealemail.com', crypt('simeonat', '6e007ce87a29470a9d87c2dae1d9728e'), '6e007ce87a29470a9d87c2dae1d9728e', jsonb_build_array('user'));
+INSERT INTO auth (id, email, passwordhash, salt, scopes) VALUES ('4b456188-75c5-4dc8-b644-7d42490cfd52', 'rocketraccoon@guardians.com', crypt('raccoon', '4b45618875c54dc8b6447d42490cfd52'), '4b45618875c54dc8b6447d42490cfd52', jsonb_build_array('user', 'admin'));
 
 INSERT INTO usertable (id, userdata) VALUES ('6e007ce8-7a29-470a-9d87-c2dae1d9728e', jsonb_build_object('username', 'SimeonAT'));
 INSERT INTO usertable (id, userdata) VALUES ('4b456188-75c5-4dc8-b644-7d42490cfd52', jsonb_build_object('username', 'RocketRaccoon'));
