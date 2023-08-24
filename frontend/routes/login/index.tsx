9d92a3df2df470313f9b963967177b0b859c43fc
Simeon Tran <siatran@ucsc.edu>
2023-08-23 20:58:00 -0700

    Creating the login page

--- frontend/routes/login/index.tsx
+++ frontend/routes/login/index.tsx
@@ -11,7 +11,9 @@ export default function LoginPage(props: PageProps) {
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-lg text-center">
-        <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
+        <h1 className="text-2xl font-bold sm:text-3xl">
+          Deno GraphQL Login
+        </h1>
 
         <p className="mt-4 text-gray-500">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla
