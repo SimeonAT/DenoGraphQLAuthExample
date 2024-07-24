3e6bfcfd5ae2706c07695d008db9aaa949c04206
Simeon Tran <siatran@ucsc.edu>
2024-07-23 23:27:54 -0700

    Created two databases

---
+++ backend/src/system/db/sql/database.sql
@@ -0,0 +1,2 @@
+SELECT 'CREATE DATABASE dev' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dev')\gexec
+SELECT 'CREATE DATABASE test' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test')\gexec
