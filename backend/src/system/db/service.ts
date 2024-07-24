d23942be6164353a4846485929f2eeec5f3f9deb
Simeon Tran <siatran@ucsc.edu>
2024-07-23 23:38:30 -0700

    Don't read database.sql

--- backend/src/system/db/service.ts
+++ backend/src/system/db/service.ts
@@ -13,13 +13,11 @@ export default class DatabaseService {
   }
 
   async initDb() {
-    const databasePath = `${Deno.cwd()}/src/system/db/sql/database.sql`;
     const schemaPath = `${Deno.cwd()}/src/system/db/sql/schema.sql`;
     const dataPath =  `${Deno.cwd()}/src/system/db/sql/data.sql`;
   
     await this.sqlReader(schemaPath);
     await this.sqlReader(dataPath);
-    await this.sqlReader(databasePath);
     return;
   }
 }
