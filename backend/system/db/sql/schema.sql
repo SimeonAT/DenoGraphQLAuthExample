c01ff9744429ef42a7ef8ea29a4fbb77d524405d
Simeon Tran <siatran@ucsc.edu>
2024-07-23 23:13:35 -0700

    REmoved extraneous files

--- backend/system/db/sql/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-DROP TABLE IF EXISTS usertable;
-DROP TABLE IF EXISTS auth;
-
-CREATE TABLE auth (id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), email VARCHAR(100) UNIQUE, passwordhash VARCHAR(64), salt VARCHAR(32), scopes jsonb);
-CREATE TABLE usertable (id uuid REFERENCES auth(id), userdata jsonb);
